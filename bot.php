a25e11fa60e4dac834edcb6863a7ea392a44b055
8abm <125156292+8abm@users.noreply.github.com>
2023-02-20 00:14:58 +0000

    Update bot.php

--- bot.php
+++ bot.php
@@ -17,31 +17,6 @@ ob_implicit_flush(1);
 $token = "6127627928:AAFUVniJndXKzQnSeIXbrIGFg49j0B9ZaaM";# توكنك تمام
 
 define('API_KEY',$token);
-
-echo "setWebhook ~> <a href=\"https://api.telegram.org/bot".API_KEY."/setwebhook?url=".$_SERVER['SERVER_NAME']."".$_SERVER['SCRIPT_NAME']."\">https://api.telegram.org/bot".API_KEY."/setwebhook?url=".$_SERVER['SERVER_NAME']."".$_SERVER['SCRIPT_NAME']."</a>";
-
-function bot($method,$datas=[]){
-
-$url = "https://api.telegram.org/bot".API_KEY."/".$method;
-
-$ch = curl_init();
-
-curl_setopt($ch,CURLOPT_URL,$url); curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
-
-curl_setopt($ch,CURLOPT_POSTFIELDS,$datas);
-
-$res = curl_exec($ch);
-
-if(curl_error($ch)){
-
-var_dump(curl_error($ch));
-
-}else{
-
-return json_decode($res);
-
-}}
-
 $update = json_decode(file_get_contents('php://input'));
 
 $message = $update->message;
