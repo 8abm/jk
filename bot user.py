bbca1a3422e2a7007e09d556b506280fb980130d
8abm <125156292+8abm@users.noreply.github.com>
2023-03-03 13:36:42 +0300

    Update bot user.py

--- bot user.py
+++ bot user.py
@@ -1,3 +1,7 @@
+import os
+os.system("pip install requests")
+os.system("pip install telebot")
+os.system("pip install Pytelegrambotapi==4")
 import requests,random
 import telebot
 myid = str("1934060947")
